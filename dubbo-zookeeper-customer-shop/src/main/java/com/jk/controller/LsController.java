d97476afcef6460f60f14b0ab8ea1744d9635cc3
wangzewei <1821024735@qq.com>
2018-07-08 00:31:07 +0800

    1113

--- dubbo-zookeeper-customer-shop/src/main/java/com/jk/controller/LsController.java
+++ dubbo-zookeeper-customer-shop/src/main/java/com/jk/controller/LsController.java
@@ -19,14 +19,13 @@ public class LsController {
     private ILsService lsService;
 
     @RequestMapping("queryUserList")
+    @ResponseBody
     public List<User> queryUserList(User user){
         List<User> list = lsService.queryUserList(user);
         return list;
     }
 
-    public void test(){}
 
-    
 
 
 
