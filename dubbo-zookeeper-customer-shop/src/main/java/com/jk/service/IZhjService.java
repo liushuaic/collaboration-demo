b3478aca9633754e7a991db74dd466754e83e2ef
zhjhope <1149056919@qq.com>
2018-07-12 23:10:27 +0800

    zhj query

--- dubbo-zookeeper-customer-shop/src/main/java/com/jk/service/IZhjService.java
+++ dubbo-zookeeper-customer-shop/src/main/java/com/jk/service/IZhjService.java
@@ -1,8 +1,7 @@
 package com.jk.service;
 
 import com.alibaba.fastjson.JSONObject;
-import com.jk.model.Product;
-import com.jk.model.ProductCategory;
+import com.jk.model.*;
 
 import java.util.List;
 
@@ -11,4 +10,16 @@ public interface IZhjService {
     JSONObject queryProductList(int page, int rows, Product pro);
 
     List<ProductCategory> queryProductCategory(String pid);
+
+    JSONObject queryPamaterList(int page, int rows, Parameter parm);
+
+    JSONObject queryAttribute(int page, int rows, Attribute att);
+
+    JSONObject querySpecificationList(int page, int rows, Specification spe);
+
+    JSONObject queryBrandList(int page, int rows, Brand brand);
+
+    JSONObject queryProductNotifyList(int page, int rows, ProductNotify pro);
+
+    List<Brand> queryBrand();
 }
